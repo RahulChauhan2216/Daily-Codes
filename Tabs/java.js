28f0bbd4027ca37fea33314e4c0d7875c568442d
Rahul <royalchauhan300@gmail.com>
2023-05-18 22:39:53 +0530

    w3 school tabs

---
+++ Tabs/java.js
@@ -0,0 +1,8 @@
+function drop(x,y,z,a,b,c) {
+    x.style.display = 'block';
+    y.style.display = 'none';
+    z.style.display = 'none';
+    a.style.backgroundColor = 'white';
+    b.style.backgroundColor = 'rgb(218, 217, 217)';
+    c.style.backgroundColor = 'rgb(218, 217, 217)';
+}
