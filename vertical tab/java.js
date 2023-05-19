b9bb4f55294c1b7fa83f1eafc26bf4dd8ddade13
Rahul <royalchauhan300@gmail.com>
2023-05-19 22:33:09 +0530

    w3 school vertical tabs

---
+++ vertical tab/java.js
@@ -0,0 +1,9 @@
+function show(x, y, z, a, b, c) {
+    x.style.display = 'block';
+    y.style.display = 'none';
+    z.style.display = 'none';
+    a.style.backgroundColor = 'rgb(154, 154, 154)';
+    b.style.backgroundColor = 'rgb(219, 218, 218)';
+    c.style.backgroundColor = 'rgb(219, 218, 218)';
+}
+
