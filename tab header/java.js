b2ef7093da35aa654b1432ea70f17ae393beb3ac
Rahul <royalchauhan300@gmail.com>
2023-05-26 15:54:16 +0530

    w3 school Tab Header

---
+++ tab header/java.js
@@ -0,0 +1,14 @@
+function show(a, b, c, d, e, color) {
+    a.style.display = 'block'
+    b.style.display = 'none'
+    c.style.display = 'none'
+    d.style.display = 'none'
+
+    let element = document.getElementsByClassName('btn');
+
+    for (let i = 0; i < element.length; i++) {
+        element[i].style.backgroundColor = '';
+    }
+
+    e.style.backgroundColor = color;
+}
